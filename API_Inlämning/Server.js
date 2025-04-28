e24d760417cb9057a1b15efffda7ba270212d8d1
Alexander Yhde <Alexander.yhde@skola.taby.se>
2025-04-28 09:52:03 +0200

    version 2

--- API_Inlämning/Server.js
+++ API_Inlämning/Server.js
@@ -6,8 +6,7 @@ const jwt = require("jsonwebtoken")
 
 // parse application/json, för att hantera att man POSTar med JSON
 const bodyParser = require("body-parser")
-
-// Inställningar av servern.
+ 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -21,7 +20,7 @@ async function getDBConnnection() {
   })
 }
 
-async function Authentication(req, res){
+async function Authentication(req, res){ // Denna funktion körs för att se ifall användaren är korrekt inloggad, igenom att validera token
     let token = req.headers['authorization']
 
     if (!token) {
@@ -48,68 +47,106 @@ app.get("/", (req, res) => {
 })
 
 app.get("/users", async function (req, res) {
-    const Authentication_success = Authentication(req, res)
-    if(!Authentication_success){return}
-
-    let connection = await getDBConnnection()
-    let response = ""
-    let sql = `SELECT * from users`   
-    let [results] = await connection.execute(sql)
-
-    for (let User = 0; User < results.length; User++){
-        response += `${User + 1}. ${results[User].Username}<br>`
+    try{
+        const Authentication_success = await Authentication(req, res)
+        if(!Authentication_success){return}
+    
+        let connection = await getDBConnnection()
+        let response = ""
+        let sql = `SELECT Username from users`   
+        let [results] = await connection.execute(sql)
+    
+        for (let User = 0; User < results.length; User++){ //Lists all users in the database
+            response += `${User + 1}. ${results[User].Username}<br>`
+        }
+      
+        res.send(response)
     }
-  
-    res.send(response)
+    catch (error){
+        res.status(500).json({ error: 'Internal server error' })
+    }
+    finally {
+        if (connection) {
+            await connection.end()
+        }
+    }
 })
 
 app.get("/users/:id", async function (req, res) {
-    const Authentication_success = Authentication(req, res)
-    if(!Authentication_success){return}
-
-    let connection = await getDBConnnection()
-    let sql = `SELECT * from users where id = ?`   
-    let [results] = await connection.execute(sql, [req.params.id])
-  
-    res.json(results[0])
+    try{
+        const Authentication_success = await Authentication(req, res)
+        if(!Authentication_success){return}
+    
+        let connection = await getDBConnnection()
+        let sql = `SELECT * from users where id = ?`   
+        let [results] = await connection.execute(sql, [req.params.id])
+    
+        if(!results[0]){res.sendStatus(204)} //Om användaren inte existerar
+        else{
+            res.json(results[0])
+        }
+    }
+    catch (error){
+        res.status(500).json({ error: 'Internal server error' })
+    }
+    finally {
+        if (connection) {
+            await connection.end()
+        }
+    }
 })
 
 //Post Request
 app.post('/users', async function (req, res){
-    const Authentication_success = Authentication(req, res)
-    if(!Authentication_success){return}
+    try{
+        async function Validate_indata_for_users(indata){ //Validerar ifall indatan är godtagbar, jämför tidigare databas värden och ser ifall indatan är unik eller inte
+            if(!indata.Username || !indata.Email || !indata.Name || !indata.Password){
+                return false //Ifall någon indata inte är angiven
+            }
 
-    function Validate_indata_for_users(indata){
-        console.log(indata)
-        if (indata.Username && indata.Password && indata.Email && indata.Name){
-            return true
+            let sql = `SELECT Username,email,name FROM users`
+            let connection = await getDBConnnection()
+    
+            let [results] = await connection.execute(sql)
+            
+            console.log(indata)
+                for (let User = 0; User < results.length; User++){
+                    if((indata.Username === results[User].Username) || (indata.Email === results[User].email) || (indata.Name === results[User].name)){
+                        return false //Indatan är inte unik och valideras som false
+                    }
+                }
+                return true //Indatan är unik och valideras som true
         }
-        else{return false}
+    
+        if (await Validate_indata_for_users(req.body)){
+            let Username = req.body.Username
+            let Name = req.body.Name
+            let Password = req.body.Password
+            let Email = req.body.Email
+    
+            let sql = `INSERT INTO users(username, name, password, email) values(?,?,?,?)`
+    
+            let connection = await getDBConnnection()
+            let [Results] = await connection.execute(sql, [Username, Name, Password, Email])
+    
+            res.json(Results)
+        }
+        else{
+            res.status(422).send("Wrong indata sent to server")
+        }
     }
-
-    console.log(Validate_indata_for_users(req.body))
-
-    if (Validate_indata_for_users(req.body)){
-        let Username = req.body.Username
-        let Name = req.body.Name
-        let Password = req.body.Password
-        let Email = req.body.Email
-
-        let connection = await getDBConnnection()
-
-        let sql = `INSERT INTO users(username, name, password, email) values(?,?,?,?)`
-
-        let [Results] = await connection.execute(sql, [Username, Name, Password, Email])
-
-        res.json(Results)
+    catch (error){
+        res.status(500).json({ error: 'Internal server error' })
     }
-    else{
-        res.status(422, "Wrong indata sent to server")
+    finally {
+        if (connection) {
+            await connection.end()
+        }
     }
 })
 
 app.put("/users/:id", async function (req, res) {
-    const Authentication_success = Authentication(req, res)
+    const Authentication_success = await Authentication(req, res)
     if(!Authentication_success){return}
 
     try{
@@ -127,68 +164,86 @@ app.put("/users/:id", async function (req, res) {
         ])
 
         if([results]){
-            console.log(res.send(results))
+            res.send(results)
         }
         else{
-            console.log(res.status(200).send(results))
+            res.status(200).send(results)
         }
-        }
+    }
     catch (error){
-        res.sendStatus(500)
-        console.log(error)
-
+        res.status(500).json({ error: 'Internal server error' })
+    }
+    finally {
+        if (connection) {
+            await connection.end()
+        }
     }
 })
 
 app.post('/register', async function(req, res) {
-
-  let connection = await getDBConnnection()
     
-  const {Name, Email, Password, Username } = req.body;
-  const salt = await bcrypt.genSalt(10);  // genererar ett salt till hashning
-  const hashedPassword = await bcrypt.hash(Password, salt); //hashar lösenordet
+    try{
+        let connection = await getDBConnnection()
+            
+        const {Name, Email, Password, Username } = req.body;
+        const salt = await bcrypt.genSalt(10);  // genererar ett salt till hashning
+        const hashedPassword = await bcrypt.hash(Password, salt); //hashar lösenordet
 
-  let sql = `UPDATE users
-  set password = ?
-  where username = ? and email = ? and name = ?
-  `
+        let sql = `UPDATE users
+        set password = ?
+        where username = ? and email = ? and name = ?
+        `
 
-  let [Results] = await connection.execute(sql, [hashedPassword, Username, Email, Name])
-  
-  res.json(Results)
+        let [Results] = await connection.execute(sql, [hashedPassword, Username, Email, Name])
+        
+        res.json(Results)
+    }   
+    catch (error){
+        res.status(500).json({ error: 'Internal server error' })
+    }
+    finally {
+        if (connection) {
+            await connection.end()
+        }
+    }
+
 });
    
 
 app.post('/login', async function(req, res) {
 
     try{
+        let connection = await getDBConnnection()
+        const {Password, Username } = req.body;
 
-    let connection = await getDBConnnection()
-    const {Password, Username } = req.body;
+        //kod här för att hantera anrop…
+        let sql = "SELECT * FROM users WHERE username = ?"
+        let [results] = await connection.execute(sql, [Username])
+        let HashedPasswordFromDB = results[0].password
 
-    //kod här för att hantera anrop…
-    let sql = "SELECT * FROM users WHERE username = ?"
-    let [results] = await connection.execute(sql, [Username])
-    let HashedPasswordFromDB = results[0].password
-    
-    // Kontrollera att det fanns en user med det username i results
+        const isPasswordValid = await bcrypt.compare(Password, HashedPasswordFromDB);
 
-    const isPasswordValid = await bcrypt.compare(Password, HashedPasswordFromDB);
+        if (isPasswordValid) {
+            let payload = {
+                sub: results[0].id, 
+                name: results[0].name 
+            }
+            let token = jwt.sign(payload, "SecretPassword", { expiresIn: '60m' })
+            res.status(200).send(token)
 
-    if (isPasswordValid) {
-        let payload = {
-            sub: results[0].id, 
-            name: results[0].name 
-          }
-        let token = jwt.sign(payload, "SecretPassword", { expiresIn: '60m' })
-        res.status(200).send(token)
-
-    } else {
-    res.status(400).json({ error: 'Invalid credentials' });
-    }
+        } 
+        else {
+            res.status(400).json({ error: 'Invalid credentials' });
+        }
     }   
-    catch{res.status(500).json({ error: 'Internal server error' })}
-  
+    catch(error){
+        res.status(500).json({ error: 'Internal server error' })
+    }
+    finally {
+        if (connection) {
+            await connection.end()
+        }
+    }
 });
    
   
